3ecfdf7d83a31caa8ad09c3074d94d562907e282
Gabriel Teles <gabriel.teles.santos@hotmail.com>
2024-04-18 10:15:55 -0300

    ajustando exercicio09.js

--- exercicio09.js
+++ exercicio09.js
@@ -37,7 +37,7 @@ switch (codOrigem) {
         regiaoProduto = 'Centro-Oeste';
         break;
     case 25: case 26: case 27: case 28: case 29: case 30: case 31: case 32: case 33: case 34: case 35: case 36: case 37: case 38: case 39: case 40: case 41: case 42: case 43: case 44: case 45: case 46: case 47: case 48: case 49: case 50:
-        regiaoProduto = 'Nordeste';
+        regiaoProduto = 'Noroeste';
         break;
     default:
         regiaoProduto = 'produto importado';
