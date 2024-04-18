2870150164ef8bf2f40cee5bde3aa914c6c96b0a
Gabriel Teles <gabriel.teles.santos@hotmail.com>
2024-04-18 12:25:47 -0300

    add exercicio10.js resolvido e add enunciado do exercicio 11 no teste.js

---
+++ exercicio10.js
@@ -0,0 +1,14 @@
+//  Atividade 01 -- Exercicio 10
+// Escreva um algoritmo para ler um número inteiro e escrevê-lo na tela 10 vezes.
+
+const prompt = require('prompt-sync')();
+
+// Área de entrada:
+let num1 = parseInt(prompt('Digite o número desejado: '));
+console.log();
+
+
+// Área de processamento e saída: 
+for (let i= 0; i < 10; i++) {
+    console.log(`O número digitado foi ${num1}.`);
+}
--- teste.js
+++ teste.js
@@ -1,9 +1,11 @@
-//  Atividade 01 -- Exercicio 09
-
+//  Atividade 01 -- Exercicio 11
+// Escreva um algoritmo para ler uma quantidade indeterminada de valores inteiros. Para
+// cada valor fornecido escrever uma mensagem que indica se cada valor fornecido é PAR
+// ou ÍMPAR. O algoritmo será encerrado imediatamente após a leitura de um valor NULO ou NEGATIVO.
 
 const prompt = require('prompt-sync')();
 
 // Área de entrada:
 
 
-// Área de processamento: 
+// Área de processamento e saída: 
