8d8b00569f98ee01ee35ea94a180a370382df4da
Gabriel Teles <gabriel.teles.santos@hotmail.com>
2024-04-19 18:53:44 -0300

    add exercicio13.js

---
+++ exercicio13.js
@@ -0,0 +1,16 @@
+//  Atividade 01 -- Exercicio 13
+// Escrever um algoritmo que leia 5 valores para uma variável N e, para cada um deles, calcule e mostre a tabuada de 1 até N. Mostre a tabuada na forma:
+// 1 x N = N
+// 2 x N = 2N
+// 3 x N = 3N
+
+const prompt = require('prompt-sync')();
+
+
+for (let i = 0; i < 5; i++){
+    let N = parseInt(prompt('Digite um número (N) para exibir a tabuada de 1 até N: '));
+    for (let i2 = 1; i2 <= N; i2++ ){
+        console.log(`${i2} x ${N} = ${i2 * N} `);
+    }
+    console.log();
+}
--- teste.js
+++ teste.js
@@ -1,13 +1,5 @@
-//  Atividade 01 -- Exercicio 12
-// Escreva um algoritmo que gere os números de 1000 a 1999 e escreva aqueles que, divididos por 11, dão resto igual a 5.
+//  Atividade 01 -- Exercicio 14
+
 
 const prompt = require('prompt-sync')();
-let contagem = 0;
 
-for (let i = 1000; i < 2000; i++){
-    if ((i % 11) === 5){
-        console.log(i)
-        contagem++
-    }
-}
-console.log(`Entre 1000 e 1999, há ${contagem} números que divididos por 11, dão resto igual a 5.`)
