3abac59cc229df6f077b1fb3901bdceacd4ebf39
Gabriel Teles <gabriel.teles.santos@hotmail.com>
2024-04-19 14:06:43 -0300

    add exercicio12.js resolvido

--- exercicio12.js
+++ exercicio12.js
@@ -1,8 +1,7 @@
 //  Atividade 01 -- Exercicio 12
 // Escreva um algoritmo que gere os números de 1000 a 1999 e escreva aqueles que, divididos por 11, dão resto igual a 5.
 
-const prompt = require('prompt-sync')();
-let contagem = 0;
+let contagemFinal = 0;
 
 for (let i = 1000; i < 2000; i++){
     if ((i % 11) === 5){
@@ -10,4 +9,4 @@ for (let i = 1000; i < 2000; i++){
         contagem++
     }
 }
-console.log(`Entre 1000 e 1999, há ${contagem} números que divididos por 11, dão resto igual a 5.`)
+console.log(`Entre 1000 e 1999, há ${contagemFinal} números que divididos por 11, dão resto igual a 5.`)
