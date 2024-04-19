500a56d865b0f4af8c80d36831065820039f9090
Gabriel Teles <gabriel.teles.santos@hotmail.com>
2024-04-19 20:12:26 -0300

    add exercicio14.js resolvido e exercicio15.js em andamento

---
+++ exercicio14.js
@@ -0,0 +1,31 @@
+//  Atividade 01 -- Exercicio 14
+// Fazer um algoritmo para receber números decimais até que o usuário digite 0 e fazer a média aritmética desses números.
+
+const prompt = require('prompt-sync')();
+
+let soma = 0;
+let quantidade = 0;
+let numero;
+
+
+console.log('=============================================================================');
+while (numero !== 0) {
+    numero = parseFloat(prompt('Digite um número (ou 0 para sair):'));
+
+    if (isNaN(numero - 1)) {
+        console.log('Você não digitou um número, repita o procedimento.');
+    }  else if (numero !== 0){
+        soma += numero;
+        quantidade++;
+    }
+}
+
+if (quantidade === 0) {
+    console.log('Nenhum número foi digitado.');
+    console.log('=============================================================================');
+} else {
+    let media = soma / quantidade;
+    console.log();
+    console.log(`A média aritmética dos números digitados é: ${media.toFixed(2)}`);
+    console.log('=============================================================================');
+}
--- teste.js
+++ teste.js
@@ -1,5 +1,4 @@
-//  Atividade 01 -- Exercicio 14
-
+//  Atividade 01 -- Exercicio 15
+//  Fazer um algoritmos para receber um número decimal e o peso de cada número até que o usuário digite o número 0. Fazer a média ponderada desses números e pesos respectivos.
 
 const prompt = require('prompt-sync')();
-
